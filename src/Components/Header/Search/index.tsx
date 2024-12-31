323116a6e79b88850917109c4d3167eba154d727
mathmarcondes <matheus.marcondes@outlook.com>
2024-12-31 13:12:36 -0300

    Add a pseudo Icon in the in the search tab

--- src/Components/Header/Search/index.tsx
+++ src/Components/Header/Search/index.tsx
@@ -1,35 +1,68 @@
-
-import { StyleSheet, TextInput, View, Keyboard, TouchableOpacity } from "react-native";
-import { Feather } from 'react-native';
-import React, {useState} from 'react';
-
-
+import React from 'react';
+import { StyleSheet, TextInput, View, TouchableOpacity, Text } from 'react-native';
+import Feather from '@react-native-vector-icons/feather';
 
 
 export default function SearchBar() {
-    return(
-        <View style={styles.container}>
-            <TextInput
-        style={{height: 40}}
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
         placeholder="Inicie sua busca"
-        />
-
+        placeholderTextColor="#555"
+      />
+       <View style={styles.content}>
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.icon}>Icone</Text>
+        </TouchableOpacity>
         </View>
-    )
+    </View>
+    
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        margin: 15,
-        justifyContent: "flex-start",
-        alignItems: "center",
-        flexDirection: "row",
-        width: "90%",
-        borderRadius: 15,
-        padding: 6,
-        backgroundColor: '#a2a29d',
-        marginEnd: 25
-    },
+  container: {
+    marginTop: 15,
+    marginRight: 20,
+    marginLeft: 15,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    width: "90%",
+    borderRadius: 25,
+    padding: 6,
+    backgroundColor: "white",
+    marginEnd: 50,
+    
 
+    // Sombra para iOS
+    shadowColor: "#000", // Cor da sombra
+    shadowOffset: { width: 2, height: 4 }, // Posição
+    shadowOpacity: 1, // Opacidade
+    shadowRadius: 4, // Raio de difusão 
+    
 
-})
+    // Sombra para Android
+    elevation: 5,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    color: "#000",
+    paddingHorizontal: 10,
+    fontSize: 16,
+  },
+  button: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  content:{
+    
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+},
+  
+});
